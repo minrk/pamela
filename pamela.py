8c7ae776af94d0836b698dc76ee82f179f861b85
Min RK <benjaminrk@gmail.com>
2019-01-17 16:20:18 +0100

    Bump version: 0.3.1.dev → 1.0.0.dev

--- pamela.py
+++ pamela.py
@@ -13,7 +13,7 @@ Implemented using ctypes, so no compilation is necessary.
 """
 from __future__ import print_function
 
-__version__ = '0.3.1.dev'
+__version__ = '1.0.0.dev'
 
 __all__ = [
     'PAMError',
